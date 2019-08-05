96fe03efe344571cea3ccc3866e9968b564f3b75
bigshow <626394369@qq.com>
2019-08-05 14:22:33 +0800

    add move animation

--- carousel.js
+++ carousel.js
@@ -4,6 +4,9 @@
     let leftButton = document.querySelector('.left');
     let rightButton = document.querySelector('.right');
     let dotList = document.querySelectorAll('.dot');
+    let timer = 800;//滑动一次图片所用时间;
+    let times = 50;//滑动一次图片经历次数
+    let timesInterval = timer/times;//滑动一次图片的中的interval间隔。
     let dotLength = dotList.length;
     let oneImgWidth = img[0].offsetWidth;
     let imgNum = img.length;
@@ -11,6 +14,8 @@
     let intervalId = null;
     let newLeft;
     let dotIndex = 0;
+    let init =true;
+    //显示选择点dot高亮函数
     function showDot(dotIndex) {
         dotList[dotIndex].className+=' dot-index';
         dotList.forEach(function (value, index) {
@@ -18,24 +23,46 @@
                 dotList[index].className='dot';
         })
     }
+    //图片每次移动动画函数
+    function moveImgInterval(oldLeft,newLeft) {
+        let t = 1;
+        let imgMoveStepLength = (newLeft-oldLeft)/times;
+        let imgIntervalId = 0;
+        imgIntervalId = setInterval(function () {
+            imgList.style.left=oldLeft+imgMoveStepLength*t+'px';
+            t++;
+            if (t>times)
+            {
+                clearInterval(imgIntervalId);
+            }
+        },timesInterval);
+    }
+    //单个图片整体自动移动函数
     function move() {
         newLeft = oldLeft - oneImgWidth;
-        imgList.style.left = newLeft+'px';
+        moveImgInterval(oldLeft,newLeft);
         oldLeft = newLeft;
         dotIndex++;
         if(dotIndex>=dotLength)
             dotIndex = 0;
         showDot(dotIndex);
     }
+    //整个图片列表自动滚动函数
     function autoRoll() {
+        if(init === true)
+        {
+            showDot(dotIndex);
+            imgList.style.left=-800+'px';
+            oldLeft = -800;
+            init = false;
+        }
         intervalId = setInterval(function () {
-            move();
-            if(oldLeft<=-(imgNum*oneImgWidth))
+            if(oldLeft<=-(imgNum-1)*oneImgWidth)
             {
-                imgList.style.left=0;
-                oldLeft = 0;
+                oldLeft = -800;
             }
-        },1200);
+            move();
+        },2000);
     }
     container.addEventListener('mouseover',function () {
        clearInterval(intervalId);
@@ -46,55 +73,53 @@
     autoRoll();
     function clickLeft()
     {
-        newLeft = oldLeft + oneImgWidth;
-        if(newLeft>0)
+        if(oldLeft>=-800)
         {
-            oldLeft = -(imgNum-1)*oneImgWidth;
-            imgList.style.left=oldLeft+'px';
+            moveImgInterval(oldLeft,oldLeft+oneImgWidth);
+            imgList.style.left=-(imgNum-2)*oneImgWidth;
+            oldLeft=-(imgNum-2)*oneImgWidth;
             dotIndex = dotLength-1;
             showDot(dotIndex);
         }
         else
         {
-            imgList.style.left=newLeft+'px';
-            oldLeft = newLeft;
+            moveImgInterval(oldLeft,oldLeft+oneImgWidth);
+            oldLeft = oldLeft+oneImgWidth;
             dotIndex--;
             showDot(dotIndex);
         }
     }
     function clickRight()
     {
-        newLeft = oldLeft - oneImgWidth;
-        if(newLeft<-(imgNum-1)*oneImgWidth)
+        if(oldLeft<=-(imgNum-2)*oneImgWidth)
         {
-            oldLeft = 0;
-            imgList.style.left=oldLeft+'px';
+            moveImgInterval(oldLeft,oldLeft-oneImgWidth);
+            imgList.style.left=-800+'px';
+            oldLeft = -800;
             dotIndex = 0;
             showDot(dotIndex);
         }
         else
         {
-            imgList.style.left=newLeft+'px';
-            oldLeft = newLeft;
+            moveImgInterval(oldLeft,oldLeft-oneImgWidth);
+            oldLeft = oldLeft-oneImgWidth;
             dotIndex++;
             showDot(dotIndex);
         }
     }
     leftButton.addEventListener('click',clickLeft);
     rightButton.addEventListener('click',clickRight);
+    //点击选择dots移动到对应图片
     function dotsClick() {
         for(let i = 0;i<dotLength;i++)
         {
 
-                setTimeout(function () {
-                    console.log(i);
-                }, 200);
+
                 dotList[i].addEventListener('click', function () {
                     dotIndex = i;
                     showDot(dotIndex);
-                    oldLeft = -(dotIndex * oneImgWidth);
-                    imgList.style.left = oldLeft + 'px';
-                    console.log(i);
+                    let desLeft = -(dotIndex+1) * oneImgWidth;
+                    moveImgInterval(desLeft+800,desLeft);
                 })
         }
     }
